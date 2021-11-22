104f25ffc898dd68082b44a05666d449512afd6d
pipeduque <juan10duque@gmail.com>
2021-11-22 09:39:41 -0500

    Base FakeDB y Persistencia

---
+++ Persistence/FabricaRepositoriosPacientes.cs
@@ -0,0 +1,28 @@
+﻿
+
+using L01_Domain.Paciente;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace L02_Persistence
+{
+    public class FabricaRepositoriosPacientes
+    {
+        public static IRepositorioPacientes CrearRepositorioPacientes()
+        {
+            var repo = ConfigurationManager.AppSettings["repository"];
+
+            return repo switch
+
+
+            {
+                "fake" => new RepositorioPacientesFake(),
+                "json" => new RepositorioPacientesJSON(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ Persistence/RepositorioEspecialidades.cs
@@ -0,0 +1,15 @@
+﻿using L01_Domain.Especialidad;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace L02_Persistence
+{
+    public class RepositorioEspecialidades : IRepositorioEspecialidades
+    {
+        List<Especialidad> IRepositorioEspecialidades.GetEspecialidades()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
