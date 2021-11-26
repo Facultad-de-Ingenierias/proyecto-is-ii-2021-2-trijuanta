a09fc48028afb1eb6ac131fb59e6c22ae7eadb33
Alejandro <50916033+alejus27@users.noreply.github.com>
2021-11-26 10:25:09 -0500

    update

--- FakeDB/TablaFamiliar.cs
+++ FakeDB/TablaFamiliar.cs
@@ -1,3 +1,42 @@
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FakeDB
+{
+    public static class TablaFamiliar
+    {
+        private static readonly List<AtributosFamiliar> familiares = new List<AtributosFamiliar>();
+        class AtributosFamiliar
+        {   
+            public string tipoDocumento { get; set; }
+            public int documento { get; set; }
+            public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public int telefono { get; set; }
+            public string direccion { get; set; }
+            public string correo { get; set; }
+
+
+
+            public AtributosFamiliar(int id)
+            {
+                tipoDocumento = "TipoDocumento" + id;
+                documento = 10000 + id;
+                nombre = "NombreFamiliarJSON" + id;
+                apellidos = "ApellidosFamiliar" + id;
+                telefono = "TelefonoFamiliar" + id;
+                direccion = "DirecionFamiliar" + id;
+                correo = "CorreoFamiliar" + id;
+
+            }
+
+            public String ToJSON()
+            {
+                return System.Text.Json.JsonSerializer.Serialize(this);
+            }
+
         }
 
 
